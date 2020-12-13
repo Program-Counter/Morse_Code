8993f3057aebee671cee55b3d282256d8fa1d0d2
Program-Counter <75812848+Program-Counter@users.noreply.github.com>
2020-12-14 00:11:39 +0100

    Update Pythagorean_Triplet.c

--- Pythagorean_Triplet.c
+++ Pythagorean_Triplet.c
@@ -18,15 +18,15 @@ int main(void)
 		puts("         PYTHAGOREAN TRIPLET\n");
 		i_c(n);
 
-    p_t_r(n);
+        p_t_r(n);
 
     	//Repeat sequence entry
 		printf("%s","\n\nTo repeat the sequence enter 'y': ");
 		
-    scanf("\n");
-    scanf("%c", &y);
+        scanf("\n");
+        scanf("%c", &y);
 		
-    puts("");
+        puts("");
 	}while(y == 'y');
 
 	return 0;
